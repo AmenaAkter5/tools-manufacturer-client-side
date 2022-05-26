215daf00e7d7c935152123b7534b039a00643c08
unknown <latapata1952@gmail.com>
2022-05-26 14:57:39 +0600

    add product outlet complete

--- src/Pages/Dashboard/MyOrders/CancelOrder.js
+++ src/Pages/Dashboard/MyOrders/CancelOrder.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import useAdmin from './../../../hooks/useAdmin';
 
 
 const CancelOrder = ({ orders, setOrders, order, setOrder }) => {
 
+    // get user state
+    const [user] = useAuthState(auth);
 
+    // get admin user
+    const [admin] = useAdmin(user);
+
+
+    // handle delete button
     const handleDelete = id => {
 
         const url = `http://localhost:5000/orders/${id}`
@@ -26,8 +36,11 @@ const CancelOrder = ({ orders, setOrders, order, setOrder }) => {
             <input type="checkbox" id="delete-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello <span className='text-green-600 font-bold'>{order.buyerName}</span></h3>
+
+                    <h3 className="font-bold text-lg">Hello <span className='text-green-600 font-bold'>{(!admin && order.buyerName) || (admin && 'Admin')}</span>,</h3>
+
                     <p className="py-4">Are You Sure to <span className='text-red-600 font-bold'>cancel</span> <span className='text-primary font-bold'>{order.tool}</span>'s order?</p>
+
                     <div className="modal-action flex justify-center">
                         <label onClick={() => handleDelete(order._id)} className='btn btn-error text-white font-bold w-50'>Yes</label>
                         <label htmlFor="delete-modal" className="btn btn-primary text-white font-bold w-50">No</label>
